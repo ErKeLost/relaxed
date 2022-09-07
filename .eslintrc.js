454fd1203e10462b0d14710b24e5f6b92c86ce35
ERKELSOT <1256029807@qq.com>
2022-09-07 18:56:02 +0800

    chore(deps): update deps

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,7 @@ module.exports = {
   root: true,
   // This tells ESLint to load the config from the package `eslint-config-custom`
   // extends: ["custom"],
+  "extends": "@relaxed",
   settings: {
     next: {
       // rootDir: ["apps/*/"],
