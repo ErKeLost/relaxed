be88c82bf69deca2c51faf25de006091fb0153ff
ERKELSOT <1256029807@qq.com>
2022-09-08 14:44:00 +0800

    chore(directives): test directives

--- packages/vue-directives/src/index.ts
+++ packages/vue-directives/src/index.ts
@@ -1,4 +1,4 @@
-import Adny from './adny'
+// import Adny from './adny'
 import { VClickoutside } from './directives/clickoutside'
 // import { VClipBoard } from './src/directives/clipboard/src/clipboard-directive'
 // import { VDebounce } from './src/directives/debounce'
@@ -7,7 +7,7 @@ import { VClickoutside } from './directives/clickoutside'
 // import { VRipple } from './src/directives/ripple/src/ripple-directive'
 // import { VThrottle } from './src/directives/throttle/src/v-throttle'
 // import { VFormatTime } from './src/directives/formattime/src/v-format-time'
-export default Adny
+// export default Adny
 export {
   VClickoutside,
   // VDebounce,
