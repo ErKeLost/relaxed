26486ea18796f74350b49e992e95ed4d3aaf2e08
ERKELSOT <1256029807@qq.com>
2022-09-11 18:24:10 +0800

    chore(sync): test zx

---
+++ script/sync.mjs
@@ -0,0 +1,15 @@
+#!/usr/bin/env zx
+import { fetch } from 'zx'
+
+const requestUrl = 'https://npmmirror.com/sync/eslint-config-relaxed'
+const requestUrlAlias = 'https://npmmirror.com/sync/@relaxed'
+
+const pkgs = ['base', 'ts', '', 'vue', 'vue2']
+
+const requestUrls = pkgs.map((item) => requestUrl + (item ? `-${item}` : ''))
+const requestUrlAliass = pkgs.map(
+  (item) => requestUrlAlias + (item ? `-${item}` : '')
+)
+
+await Promise.all(requestUrls.map((url) => fetch(url)))
+await Promise.all(requestUrlAliass.map((url) => fetch(url)))
