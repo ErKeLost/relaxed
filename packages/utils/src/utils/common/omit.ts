477ec85b29f3aa248c523c4fdacbe1418292d217
ERKELSOT <1256029807@qq.com>
2022-12-18 08:25:35 +0800

    chore: omit

---
+++ packages/utils/src/utils/common/omit.ts
@@ -0,0 +1,14 @@
+function omit<T extends Record<string, unknown>, K extends keyof T>(
+  obj: T,
+  fields: K[]
+): Omit<T, K> {
+  // eslint-disable-next-line prefer-object-spread
+  const shallowCopy = Object.assign({}, obj)
+  for (let i = 0; i < fields.length; i += 1) {
+    const key = fields[i]
+    delete shallowCopy[key]
+  }
+  return shallowCopy
+}
+export { omit }
+export default omit
