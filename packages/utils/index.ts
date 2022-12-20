8bb471e447369739e8d5d33e5e290635d1f0963c
ERKELSOT <1256029807@qq.com>
2022-12-20 22:52:17 +0800

    feat: structure with relaxed/utils

--- packages/utils/index.ts
+++ packages/utils/index.ts
@@ -1,13 +1,6 @@
-// export * from "./utils/color/color";
-// export { DOMUtils } from "./dom/drag/draggable";
-
-// export * from "./types/shared";
-// export * from "./storage";
 export * from './src/utils'
 export * from './src/types'
 export * from './src/storage'
 export * from './src/dom'
 export * from './src/date'
-export * from '../shared'
-export * from './src/dom/share-utils'
-export { Tween } from './src/animation/tween'
+export * from './src/animation/tween'
--- packages/utils/src/utils/index.ts
+++ packages/utils/src/utils/index.ts
@@ -1,8 +1,7 @@
-import { debounce } from './debounce'
-import { throttle } from './throttle'
-import { deepClone } from './deepclone'
-import { formatDuration, formatCount } from './format'
-export { debounce, throttle, deepClone, formatDuration, formatCount }
+export * from './debounce'
+export * from './throttle'
+export * from './deepclone'
+export * from './format'
 export * from './color/color'
 export * from './color/color-utils'
 export * from './color/color-types'
