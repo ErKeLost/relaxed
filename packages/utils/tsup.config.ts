9352d014dbb9b4cf3ff9b58d31c97d2c94c68228
ERKELSOT <1256029807@qq.com>
2022-12-21 19:40:30 +0800

    chore: build

--- packages/utils/tsup.config.ts
+++ packages/utils/tsup.config.ts
@@ -2,10 +2,9 @@ import { defineConfig } from 'tsup'
 
 export default defineConfig({
   entry: ['index.ts'],
-  target: 'esnext',
-  format: ['esm', 'cjs', 'iife'],
+  // target: 'esnext',
+  format: ['esm', 'cjs'],
   splitting: false,
   sourcemap: true,
-  clean: true,
-  external: ['vue']
+  clean: true
 })
