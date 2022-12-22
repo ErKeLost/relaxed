bb5dd2b3777559aa3b54d88d81f43d69a317654c
ERKELSOT <1256029807@qq.com>
2022-12-22 16:40:51 +0800

    chore: build

--- playground/src/App.vue
+++ playground/src/App.vue
@@ -1,21 +1,21 @@
 <template>
-  <RelaxedLayout fixed-sider>
+  <RelaxedLayout>
     <template #header>
-      <div class="flex-center h-full bg-#bfd">Header</div>
+      <div class="flex-center h-full bg-#D0A245FF">Header</div>
     </template>
     <template #tab>
       <div class="flex-center h-full bg-#6DCB6C">Tab</div>
     </template>
     <template #sider>
       <div class="h-full bg-#6CCBC8">
-        <div class="flex-center h-56px">vue-directiveSider</div>
+        <div class="flex-center h-56px">Sider</div>
       </div>
     </template>
     <template #footer>
-      <div class="flex-center h-full bg-#bfa">Footer</div>
+      <div class="flex-center h-full bg-#D061D8">Footer</div>
     </template>
-    <div class="text-center bg-#D061D8">
-      <div v-for="i in 50" :key="i" class="text-center">{{ i }}</div>
+    <div v-for="i in 50" :key="i" class="text-center">
+      {{ i }}
     </div>
   </RelaxedLayout>
 </template>
