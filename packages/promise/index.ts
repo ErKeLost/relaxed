26104c88a5c0a4983fb94af1fab1dfdfba3b9268
erkelost <1256029807@qq.com>
2023-03-22 13:41:20 +0800

    feat: try plimit

--- packages/promise/index.ts
+++ packages/promise/index.ts
@@ -85,3 +85,50 @@ export function sleep(ms: number, callback?: Fn<any>) {
     }, ms)
   )
 }
+
+/**
+ *
+ * @param
+ */
+
+export const pLimit2 = (concurrency) => {
+  const queue = []
+  let activeCount = 0
+
+  const next = () => {
+    activeCount--
+
+    if (queue.length > 0) {
+      queue.shift()()
+    }
+  }
+
+  const run = async (fn, resolve, ...args) => {
+    activeCount++
+
+    const result = (async () => fn(...args))()
+
+    resolve(result)
+
+    try {
+      await result
+    } catch {}
+
+    next()
+  }
+
+  const enqueue = (fn, resolve, ...args) => {
+    queue.push(run.bind(null, fn, resolve, ...args))
+
+    if (activeCount < concurrency && queue.length > 0) {
+      queue.shift()()
+    }
+  }
+
+  const generator = (fn, ...args) =>
+    new Promise((resolve) => {
+      enqueue(fn, resolve, ...args)
+    })
+
+  return generator
+}
